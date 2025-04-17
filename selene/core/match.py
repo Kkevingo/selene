c974dec3b39f62851056e1701f388541159c5215
Kkevingo <k.ngo@student.tue.nl>
2025-04-17 19:55:26 +0200

    [#566] STYLE: Format code with Black

--- selene/core/match.py
+++ selene/core/match.py
@@ -353,10 +353,9 @@ selected: Condition[Element] = Match(
 
 not_overlapped: Condition[Element] = Match(
     'is not overlapped',
-    by=lambda element: (
-        element._actual_not_overlapped_webelement and True
-    ),
+    by=lambda element: (element._actual_not_overlapped_webelement and True),
 )
+
 
 class js:
     # todo: how will it work for mobile? – it will not work:)
